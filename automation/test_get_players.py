0b3832aa1a78f2e1ba9696b89b78934ee7b54eba
yuvalyanous1 <yuvaly612@gmail.com>
2023-11-02 15:22:57 +0200

    improve

--- automation/test_get_players.py
+++ automation/test_get_players.py
@@ -16,3 +16,26 @@ def test_read_player(base_url:str):
         print("response status code:", response.status_code)
 
 
+def test_get_random(base_url:str):
+
+    endpoint = "/"
+    url = base_url + endpoint
+    r = requests.get(url)
+    data = json.loads(r.text)
+
+    players_list = []
+
+    for players in data:
+        player_name = players ['player_name']
+
+        player_item = {
+            'player_name': player_name
+        }
+        players_list.append(player_item)
+
+
+    print(players_list)
+
+
+
+
